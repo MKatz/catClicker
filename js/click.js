6bfe607369d190ada551549d24be84691ddcb335
Matt Katz <mrmattkatz@gmail.com>
2015-09-28 14:04:25 -0500

    First Commit

---
+++ js/click.js
@@ -0,0 +1,60 @@
+$('#targetFreak').click(function() {
+    $('#outputFreak').html(function(i, val) {
+        return val * 1 + 1
+    });
+});
+$('#targetChill').click(function() {
+    $('#outputChill').html(function(i, val) {
+        return val * 1 + 1
+    });
+});
+$('#targetHappy').click(function() {
+    $('#outputHappy').html(function(i, val) {
+        return val * 1 + 1
+    });
+});
+$('#targetGrumpy').click(function() {
+    $('#outputGrumpy').html(function(i, val) {
+        return val * 1 + 1
+    });
+});
+$('#targetFlying').click(function() {
+    $('#outputFlying').html(function(i, val) {
+        return val * 1 + 1
+    });
+});
+$('#btnFreak').click(function() {
+    $('#divFreak').show("slow");
+    $('#divChill').hide('slow');
+    $('#divHappy').hide('slow');
+    $('#divGrumpy').hide('slow');
+    $('#divFlying').hide('slow');
+});
+$('#btnChill').click(function() {
+    $('#divChill').show("slow");
+    $('#divFreak').hide('slow');
+    $('#divHappy').hide('slow');
+    $('#divGrumpy').hide('slow');
+    $('#divFlying').hide('slow');
+});
+$('#btnHappy').click(function() {
+    $('#divHappy').show("slow");
+    $('#divChill').hide('slow');
+    $('#divFreak').hide('slow');
+    $('#divGrumpy').hide('slow');
+    $('#divFlying').hide('slow');
+});
+$('#btnGrumpy').click(function() {
+    $('#divGrumpy').show("slow");
+    $('#divChill').hide('slow');
+    $('#divHappy').hide('slow');
+    $('#divFreak').hide('slow');
+    $('#divFlying').hide('slow');
+});
+$('#btnFlying').click(function() {
+    $('#divFlying').show("slow");
+    $('#divChill').hide('slow');
+    $('#divHappy').hide('slow');
+    $('#divGrumpy').hide('slow');
+    $('#divFreak').hide('slow');
+});
